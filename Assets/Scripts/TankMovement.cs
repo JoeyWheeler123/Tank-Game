68dc4aa53188d4cb769fb37b4c81acebce9de656
Connor Ryan <C16755215@mydit.ie>
2018-09-21 17:04:49 +0100

    initial version

---
+++ Assets/Scripts/TankMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankMovement : MonoBehaviour {
+
+    public GameObject bulletCreate;
+    public Transform bulletLo;
+
+    public float force;
+    public float speed;
+    public float rotate;
+	
+	void Start ()
+    {
+
+	}
+	
+	
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward* speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * rotate * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down * rotate * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+
+    }
+
+    void Shoot ()
+    {
+        var bullet = (GameObject)Instantiate(bulletCreate, bulletLo);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * force;
+        Destroy(bullet, 5.0f);
+    }
+}
---
+++ Assets/Scripts/WallSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpawner : MonoBehaviour {
+
+    public GameObject cube;
+
+    //public Transform cubeLo;
+
+    void Start ()
+    {
+		for (int i = 0; i < 5; i++)
+        {
+            for( int x = 0; x < 5; x++)
+            {
+                GameObject clone;
+                clone = Instantiate(cube, new Vector3(i * 3.0f, x * 3.0f, 0), Quaternion.identity);
+                clone.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 1f, 1f);
+            }
+        }
+	}
+	
+	
+	void Update () {
+		
+	}
+}
